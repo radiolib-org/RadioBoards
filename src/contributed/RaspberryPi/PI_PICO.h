9a651123775e79d6729f9105827e2467d0a954a2
K4KDR <scott23192+github@gmail.com>
2024-06-03 14:34:49 +0000

    Update PI_PICO.h
    updated to include confirmed good SPI settings & commands for this board

--- src/contributed/RaspberryPi/PI_PICO.h
+++ src/contributed/RaspberryPi/PI_PICO.h
@@ -3,6 +3,7 @@
 
 // sources:
 // https://datasheets.raspberrypi.com/pico/Pico-R3-A4-Pinout.pdf
+// https://github.com/jgromes/RadioLib/issues/729
 
 #define RADIO_BOARDS_NAME "Raspberry Pi Pico"
 
@@ -10,5 +11,19 @@
 #define RADIO_IRQ     (20)
 #define RADIO_RST     (15)
 #define RADIO_GPIO    (2)
+
+// this board uses custom SPI to interface with the module
+#define RADIO_SPI     SPI1
+#define RADIO_MISO    (12)
+#define RADIO_MOSI    (11)
+#define RADIO_SCK     (10)
+
+#define RADIO_SPI_INIT                                    \
+SPI1.setSCK(10);         \       // from https://github.com/jgromes/RadioLib/issues/729
+SPI1.setTX(11);          \       // ~
+SPI1.setRX(12);          \       // ~  
+pinMode(3, OUTPUT);      \       // ~
+digitalWrite(3, HIGH);   \       // ~
+RADIO_SPI1.begin(false);         // from https://github.com/jgromes/RadioLib/issues/729
 
 #endif
