769e18f0c5f91d21fea65aed4115856dbff290c5
K4KDR <scott23192+github@gmail.com>
2024-06-03 17:37:36 +0000

    Update PI_PICO.h
    attempted to make requested changes.    HUGE thanks for your patience as I am only familiar w/ a small percentage of what is proper syntax.   However, with each post you all share a tremendous amount of education to anyone who sees these updates, so thank you!

--- src/contributed/RaspberryPi/PI_PICO.h
+++ src/contributed/RaspberryPi/PI_PICO.h
@@ -7,6 +7,10 @@
 
 #define RADIO_BOARDS_NAME "Raspberry Pi Pico"
 
+#if RADIOLIB_SUPPORT_ENABLED
+  #define Radio       SX1262
+#endif
+
 #define RADIO_NSS     (3)   // i.e., SX1262 radio = new Module(3, 20, 15, 2, SPI1, RADIOLIB_DEFAULT_SPI_SETTINGS);
 #define RADIO_IRQ     (20)
 #define RADIO_RST     (15)
@@ -18,12 +22,10 @@
 #define RADIO_MOSI    (11)
 #define RADIO_SCK     (10)
 
-#define RADIO_SPI_INIT                                    \
-SPI1.setSCK(10);         \       // from https://github.com/jgromes/RadioLib/issues/729
-SPI1.setTX(11);          \       // ~
-SPI1.setRX(12);          \       // ~  
-pinMode(3, OUTPUT);      \       // ~
-digitalWrite(3, HIGH);   \       // ~
-RADIO_SPI1.begin(false);         // from https://github.com/jgromes/RadioLib/issues/729
+#define RADIO_SPI_INIT          \
+RADIO_SPI.setSCK(RADIO_SCK);    \
+RADIO_SPI.setTX(RADIO_MOSI);    \
+RADIO_SPI.setRX(RADIO_MISO);    \
+RADIO_SPI.begin(false);
 
 #endif
